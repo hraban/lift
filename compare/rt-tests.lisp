ed96baab28ec917fa1bd359259380779699ead88
Gary King <gwking@metabang.com>
2006-01-26 23:03:39 -0500

    improving comparison
    darcs-hash:20060127040339-3cc5d-c3c1a1cfaea106b159e5cd1a19f573fa87740bcd.gz

--- compare/rt-tests.lisp
+++ compare/rt-tests.lisp
@@ -1,6 +1,5 @@
 #|
-non-interactive
-can you change the test
+can you change the equality test?
 |#
 
 (in-package #:cl)
@@ -10,47 +9,8 @@ can you change the test
 
 (rem-all-tests)
 
-
 (deftest :str.0 (substitute-chars-strings "" nil) "")
 (deftest :str.1 (substitute-chars-strings "abcd" nil) "abcd")
-(deftest :str.2 (substitute-chars-strings "abcd" nil) "abcd")
-(deftest :str.3 (substitute-chars-strings "abcd" '((#\j . "ef"))) "abcd")
-(deftest :str.4 (substitute-chars-strings "abcd" '((#\a . "ef"))) "efbcd")
-(deftest :str.5
-    (substitute-chars-strings "abcd" '((#\a . "ef") (#\j . "ghi")))
-  "efbcd")
-(deftest :str.6
-    (substitute-chars-strings "abcd" '((#\a . "ef") (#\d . "ghi")))
-  "efbcghi")
+(deftest :str.2 (substitute-chars-strings "abcd" nil) "abcde")
 
-(deftest :str.7 (escape-xml-string "") "")
-(deftest :str.8 (escape-xml-string "abcd") "abcd")
-(deftest :str.9 (escape-xml-string "ab&cd") "ab&amp;cd")
-(deftest :str.10 (escape-xml-string "ab&cd<") "ab&amp;cd&lt;")
-(deftest :str.12 (string-trim-last-character "") "")
-(deftest :str.13 (string-trim-last-character "a") "")
-(deftest :str.14 (string-trim-last-character "ab") "a")
-(deftest :str.15 (nstring-trim-last-character "") "")
-(deftest :str.16 (nstring-trim-last-character "a") "")
-(deftest :str.17 (nstring-trim-last-character "ab") "a")
-
-(deftest :str.18 (delimited-string-to-list "ab|cd|ef" #\|)
-					  ("ab" "cd" "ef"))
-(deftest :str.19 (delimited-string-to-list "ab|cd|ef" #\| t)
-					  ("ab" "cd" "ef"))
-(deftest :str.20 (delimited-string-to-list "") (""))
-(deftest :str.21 (delimited-string-to-list "" #\space t) (""))
-(deftest :str.22 (delimited-string-to-list "ab") ("ab"))
-(deftest :str.23 (delimited-string-to-list "ab" #\space t) ("ab"))
-(deftest :str.24 (delimited-string-to-list "ab|" #\|) ("ab" ""))
-(deftest :str.25 (delimited-string-to-list "ab|" #\| t) ("ab"))
-
-(deftest :sdstl.1 (string-delimited-string-to-list "ab|cd|ef" "|a")
-  ("ab|cd|ef"))
-(deftest :sdstl.2 (string-delimited-string-to-list "ab|cd|ef" "|")
-  ("ab" "cd" "ef"))
-(deftest :sdstl.3 (string-delimited-string-to-list "ab|cd|ef" "cd")
-  ("ab|" "|ef"))
-(deftest :sdstl.4 (string-delimited-string-to-list "ab|cd|ef" "ab")
-  ("" "|cd|ef"))
-
+(do-tests)
--- compare/xlunit-tests.lisp
+++ compare/xlunit-tests.lisp
@@ -1,10 +1,3 @@
-#|
-How can you run all of the tests in a hierarchy?
-Verbose
-
-Times the tests
-|#
-
 (in-package #:cl)
 (defpackage #:kmrcl-tests-xlunit
   (:use #:kmrcl #:cl #:xlunit))
@@ -16,14 +9,7 @@ Times the tests
   (assert-true (string-equal (substitute-chars-strings "" nil) "")))
 (def-test-method  :str.1 ((test string-test-case) :run nil)
   (assert-true (string-equal (substitute-chars-strings "abcd" nil) "abcd")))
-
-
-(defclass string-delimited-string-to-list-test-case (string-test-case)
-  ())
-(def-test-method :str.0 ((test string-delimited-string-to-list-test-case) :run nil)
-  (assert-true (equal
-                (string-delimited-string-to-list "ab|cd|ef" "|a")
-                '("ab|cd|ef"))))
+(def-test-method  :str.1 ((test string-test-case) :run nil)
+  (assert-true (string-equal (substitute-chars-strings "abcd" nil) "abcde")))
 
 (textui-test-run (get-suite string-test-case))
-(textui-test-run (get-suite string-delimited-string-to-list-test-case))
