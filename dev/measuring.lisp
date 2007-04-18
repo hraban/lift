6bd2283d19fa3849d9bb6ebeaf7aae867fbbe01e
Gary King <gwking@metabang.com>
2007-04-18 18:32:22 -0400

    added measure as a macro and with-profiling-report (allegro only for now)
    darcs-hash:20070418223222-3cc5d-b10cb728e1b6087385beab3d0b5bc2122f403706.gz

--- dev/measuring.lisp
+++ dev/measuring.lisp
@@ -19,7 +19,7 @@
   `(with-measuring (,var total-bytes-allocated)
      ,@body))
 
-(defun measure (fn &rest args)
+(defun measure-fn (fn &rest args)
   (declare (dynamic-extent args))
   (let ((bytes 0) (seconds 0) result)
     (measure-time (seconds)
@@ -27,6 +27,84 @@
 	(setf result (apply fn args))))
     (values seconds bytes result)))
 
+(defmacro measure (seconds bytes &body body)
+  (let ((result (gensym)))
+    `(let (,result)
+       (measure-time (,seconds)
+	 (measure-conses (,bytes)
+	   (setf ,result ,@body)))
+       (values ,result))))
+
+(defparameter *benchmark-file*
+  (asdf:system-relative-pathname 
+   'lift "benchmark-data/benchmarks.log"))
+
+(defvar *additional-markers* nil)
+
+#+allegro
+(defun cancel-current-profile (&key force?)
+  (unless force?
+    (assert (member (prof:profiler-status) '(:inactive))))
+  (prof:stop-profiler)
+  (setf prof::*current-profile* (prof::make-current-profile)))
+
+#+allegro
+(defun current-profile-sample-count ()
+  (ecase (prof::profiler-status)
+    ((:inactive :analyzed) 0)
+    ((:suspended :saved)
+     (slot-value (prof::current-profile-actual prof::*current-profile*) 
+		 'prof::samples))
+    (:sampling (warn "Can't determine count while sampling"))))
+
+#+allegro
+(defmacro with-profile-report ((name style &key (log-name *benchmark-file*)) 
+			       &body body)
+  (assert (member style '(:time :space)))
+  `(let ((seconds 0.0) (conses 0) result)
+     (cancel-current-profile)
+     (multiple-value-prog1
+	 (prof:with-profiling (:type ,style)
+	   (measure seconds conses ,@body))
+       (let ((date-stamp (get-universal-time)))
+	 (ensure-directories-exist ,log-name)
+	 ;;log 
+	 (with-open-file (output ,log-name
+				 :direction :output
+				 :if-does-not-exist :create
+				 :if-exists :append)
+	   (with-standard-io-syntax
+	     (let ((*print-readably* nil))
+	       (terpri output)
+	       (format output "\(~11,d ~20,s ~10,s ~10,s ~{~s~^ ~} ~s\)"
+		       date-stamp ,name 
+		       seconds conses *additional-markers*
+		       result))))
+	 (when (> (current-profile-sample-count) 0)
+	   (let ((pathname (merge-pathnames
+			    (make-pathname 
+			     :type "prof"
+			     :name (format nil "~a-~a-~a"
+					   ,name ,style date-stamp))
+			    ,log-name)))
+	     (format t "~&Profiling output being sent to ~a" pathname)
+	     (with-open-file (output pathname
+				     :direction :output
+				     :if-does-not-exist :create
+				     :if-exists :append)
+	       (format output "~&Profile data for ~a" ,name)
+	       (format output "~&Date: ~a" 
+		       (excl:locale-print-time (get-universal-time)
+					       :fmt "%B %d, %Y %T" :stream nil))
+	       (format output "~&  Total time: ~,2F; Total space: ~:d \(~:*~d\)"
+		       seconds conses)
+	       (format output "~%~%")
+	       (when (or (eq :time ,style)
+			 (eq :space ,style))
+		 (prof:show-flat-profile :stream output)
+		 (prof:show-call-graph :stream output)))))))))
+
+#| OLD
 ;; integrate with LIFT
 
 (pushnew :measure *deftest-clauses*)
@@ -44,90 +122,4 @@
 		 :accessor total-conses)
    (total-seconds :initform 0
 		  :accessor total-seconds)))
-
-(defmethod do-testing :around ((suite measured-test-mixin) result fn)
-  (declare (ignore fn))
-  (setf (values (total-seconds suite) (total-conses suite))
-	(measure #'call-next-method))
-  (values result (total-seconds suite) (total-conses suite)))
-
-#|
-
-sample 
-
-(deftestsuite test-the-foo ()
-  ()
-  :measure t)
-
-(addtest (test-the-foo)
-  (ensure 23)
-  (sleep 0.5)
-  (ensure 1))
-
 |#
-
-(defparameter *benchmark-file*
-  (asdf:system-relative-pathname 'lift
-			    "benchmark-data/benchmarks.log"))
-
-(defparameter *profiling* nil)
-
-(defvar *scratch-directory*
-  (asdf:system-relative-pathname 'lift "scratch-directory/"))
-
-(defvar *additional-markers* nil)
-
-#+allegro
-(defun benchmark (name fn &rest args)
-  (declare (dynamic-extent args))
-  (let ((seconds 0.0) (conses 0) result)
-    (flet ((do-it ()
-	     (setf (values seconds conses result)
-		   (apply 'measure fn args))))
-      (cond ((or (eq :time *profiling*)
-		 (eq :space *profiling*))
-	     (prof:with-profiling (:type *profiling*) (do-it)))
-	    ((eq :count *profiling*)
-	     (prof:with-profiling (:count t) (do-it)))
-	    (t
-	     (do-it)))
-      (let ((date-stamp (get-universal-time)))
-	(ensure-directories-exist *benchmark-file*)
-	;;log 
-	(with-open-file (output *benchmark-file* 
-				:direction :output
-				:if-does-not-exist :create
-				:if-exists :append)
-	  (with-standard-io-syntax
-	    (let ((*print-readably* nil))
-	      (terpri output)
-	      (format output "\(~11,d ~20,s ~10,s ~10,s ~{~s~^ ~}~30,s ~s ~s\)"
-		      date-stamp name 
-		      seconds conses *additional-markers*
-		      (if (symbolp fn) fn "<function>") args result))))
-	(when *profiling*
-	  (let ((pathname (merge-pathnames
-			   (make-pathname 
-			    :type "prof"
-			    :name (format nil "~a-~a-~a"
-					  name *profiling* date-stamp))
-			   *benchmark-file*)))
-	    (format t "~&Profiling output being sent to ~a" pathname)
-	    (with-open-file (output pathname
-				    :direction :output
-				    :if-does-not-exist :create
-				    :if-exists :append)
-	      (format output "~&Profile data for ~a" name)
-	      (format output "~&  Total time: ~,2F; Total space: ~:d \(~d\)"
-		      seconds conses conses)
-	      (format output "~&  Arguments:~{~&    ~s,~^~}" args)
-	      (format output "~%~%")
-	      (when (or (eq :time *profiling*)
-			(eq :space *profiling*))
-		(prof:show-flat-profile :stream output)
-		(prof:show-call-graph :stream output))
-	      (when (eq :count *profiling*)
-		(let ((*standard-output* output)
-		      (*print-readably* nil))
-		  (prof:show-call-counts)))))))
-      (list seconds conses result))))
