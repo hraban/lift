8ea899ad90b7d7abd01825b07f6dd77667bbc9a1
Gary King <gwking@metabang.com>
2007-07-19 19:40:28 -0400

    Added :include command for configuration files
    darcs-hash:20070719234028-3cc5d-4610895f996a594e8b4af360691acd4242fe9878.gz

--- dev/config.lisp
+++ dev/config.lisp
@@ -18,75 +18,54 @@
 		    *current-asdf-system-name*
 		    "lift-standard.config"))))
   (setf *test-result*
-	(let ((*package* *package*)
-	      (*read-eval* nil)
-	      (form nil)
-	      (result (make-test-result path :multiple)))
-	  (with-open-file (in path
-			      :direction :input
-			      :if-does-not-exist :error)
-	    (let ((*lift-dribble-pathname* nil)
-		  (*lift-debug-output* *debug-io*)
-		  (*lift-standard-output* *standard-output*)
-		  (*test-break-on-errors?* nil)
-		  (*test-do-children?* t)
-		  (*lift-equality-test* 'equal)
-		  (*test-print-length* :follow-print)
-		  (*test-print-level* :follow-print)
-		  (*lift-if-dribble-exists* :append)
-		  (*test-result* result))
-	      (loop while (not (eq (setf form (read in nil :eof nil)) :eof)) 
-		 collect
-		   (handler-bind 
-		       ((error (lambda (c) (format 
-			       *error-output* 
-			       "Error while running ~a from ~a: ~a"
-			       form path c)
-			  (invoke-debugger c))))
-		     (destructuring-bind
-			   (name &rest args &key &allow-other-keys)
-			 form
-		       (assert (typep name 'symbol))
-		       (setf args (massage-arguments args))
-		       (cond 
-			 ;; check for preferences first (i.e., keywords)
-			 ((eq (symbol-package name) 
-			      (symbol-package :keyword))
-			  ;; must be a preference
-			  (handle-config-preference name args))
-			 ((subtypep (find-testsuite name)
-				    'lift:test-mixin)
-			  (apply #'run-tests :suite name 
-				 :result result args))
-			 (t
-			  (error "Don't understand '~s' while reading from ~s" 
-				 form path)))))
-		   #+(or)
-		   (handler-case 
-		     (destructuring-bind
-			   (name &rest args &key &allow-other-keys)
-			 form
-		       (assert (typep name 'symbol))
-		       (setf args (massage-arguments args))
-		       (cond 
-			 ;; check for preferences first (i.e., keywords)
-			 ((eq (symbol-package name) 
-			      (symbol-package :keyword))
-			  ;; must be a preference
-			  (handle-config-preference name args))
-			 ((subtypep (find-testsuite name)
-				    'lift:test-mixin)
-			  (apply #'run-tests :suite name 
-				 :result result args))
-			 (t
-			  (error "Don't understand '~s' while reading from ~s" 
-				 form path))))
-		   (error (c) (format 
-			       *error-output* 
-			       "Error while running ~a from ~a: ~a"
-			       form path c)
-			  (invoke-debugger c))))))
-	  (values result))))
+	(let* ((*package* *package*)
+	       (*read-eval* nil)
+	       (result (make-test-result path :multiple))
+	       (*lift-dribble-pathname* nil)
+	       (*lift-debug-output* *debug-io*)
+	       (*lift-standard-output* *standard-output*)
+	       (*test-break-on-errors?* nil)
+	       (*test-do-children?* t)
+	       (*lift-equality-test* 'equal)
+	       (*test-print-length* :follow-print)
+	       (*test-print-level* :follow-print)
+	       (*lift-if-dribble-exists* :append)
+	       (*test-result* result))
+	  (%run-tests-from-file path))))
+
+(defun %run-tests-from-file (path)
+  (with-open-file (*current-configuration-stream* path
+		      :direction :input
+		      :if-does-not-exist :error)
+    (let ((form nil))
+      (loop while (not (eq (setf form (read *current-configuration-stream* 
+					    nil :eof nil)) :eof)) 
+	 collect
+	 (handler-bind 
+	     ((error (lambda (c) (format 
+				  *error-output* 
+				  "Error while running ~a from ~a: ~a"
+				  form path c)
+			     (invoke-debugger c))))
+	   (destructuring-bind
+		 (name &rest args &key &allow-other-keys)
+	       form
+	     (assert (typep name 'symbol))
+	     (setf args (massage-arguments args))
+	     (cond 
+	       ;; check for preferences first (i.e., keywords)
+	       ((eq (symbol-package name) 
+		    (symbol-package :keyword))
+		;; must be a preference
+		(handle-config-preference name args))
+	       ((subtypep (find-testsuite name)
+			  'lift:test-mixin)
+		(apply #'run-tests :suite name 
+		       :result *test-result* args))
+	       (t
+		(error "Don't understand '~s' while reading from ~s" 
+		       form path))))))))
+  (values *test-result*))
 
 (defun massage-arguments (args)
   (loop for arg in args collect
@@ -96,6 +75,10 @@
 (defmethod handle-config-preference ((name t) args)
   (error "Unknown preference ~s (with arguments ~s)" 
 	 name args))
+
+(defmethod handle-config-preference ((name (eql :include)) args)
+  (%run-tests-from-file (merge-pathnames (first args) 
+					 *current-configuration-stream*)))
 
 (defmethod handle-config-preference ((name (eql :dribble)) args)
   (setf *lift-dribble-pathname* (first args)))
