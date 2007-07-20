bf656ba2e58b64c05198892786e0f4e5fb03ca4d
Gary King <gwking@metabang.com>
2007-07-20 10:34:19 -0400

    More mucking to remove reliance on system-relative-pathname
    darcs-hash:20070720143419-3cc5d-a20472565083cdde485a35f6e973e060397e18fa.gz

--- dev/packages.lisp
+++ dev/packages.lisp
@@ -19,22 +19,28 @@
        #:with-profile-report))))
 
 (unless (and (find-package :asdf)
-	     (intern (symbol-name 'system-relative-pathname) :asdf))
+	     (find-symbol (symbol-name 'system-relative-pathname) :asdf)
+	     (fboundp (find-symbol
+		       (symbol-name 'system-relative-pathname) :asdf)))
   (warn "LIFT uses asdf:system-relative-pathname which your version of ASDF 
 doesn't seem to include. LIFT will define these for now but you may want to consider updating to the most recent version of ASDF (see http://www.cliki.net/asdf for details).")
-  (defun asdf:system-source-file (system-name)
+  (intern (symbol-name 'system-source-file) :asdf)
+  (intern (symbol-name 'system-source-directory) :asdf)
+  (intern (symbol-name 'system-relative-pathname) :asdf)
+  (export 'asdf::system-relative-pathname :asdf) 
+  (defun asdf::system-source-file (system-name)
     (let ((system (asdf:find-system system-name)))
       (make-pathname 
        :type "asd"
        :name (asdf:component-name system)
        :defaults (asdf:component-relative-pathname system))))
 
-  (defun asdf:system-source-directory (system-name)
+  (defun asdf::system-source-directory (system-name)
     (make-pathname :name nil
 		   :type nil
-		   :defaults (asdf:system-source-file system-name)))
+		   :defaults (asdf::system-source-file system-name)))
 
-  (defun asdf:system-relative-pathname (system pathname &key name type)
+  (defun asdf::system-relative-pathname (system pathname &key name type)
     (let ((directory (pathname-directory pathname)))
       (when (eq (car directory) :absolute)
 	(setf (car directory) :relative))
@@ -42,5 +48,5 @@ doesn't seem to include. LIFT will define these for now but you may want to cons
        (make-pathname :name (or name (pathname-name pathname))
 		      :type (or type (pathname-type pathname))
 		      :directory directory)
-       (asdf:system-source-directory system)))))
+       (asdf::system-source-directory system)))))
   
