e5626bb8302c99c24bff7a57bd57c118232bd5d7
Gary King <gwking@franz.com>
2010-07-10 21:01:58 -0400

    Fix for ensure-error and SBCL from Tamas Papp (thanks!)

--- dev/macros.lisp
+++ dev/macros.lisp
@@ -361,8 +361,8 @@ the signaling of _any_ other condition will cause a test failure.
 "
   (setf condition (remove-leading-quote condition))
   (destructuring-bind (condition &key report arguments catch-all-conditions?
-				 validate name)
-                      (if (consp condition) condition (list condition))
+				 validate (name (gensym (symbol-name '#:name-))))
+      (if (consp condition) condition (list condition))
     (let ((g (gensym)))
       `(let ((,g nil))
          (unwind-protect
